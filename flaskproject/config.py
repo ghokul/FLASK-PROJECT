8d4ea403d66c17af8e33d90db3ec07f9a57484ce
ghokul <66864130+ghokul@users.noreply.github.com>
2020-09-05 20:27:46 +0530

    This is my first web application

---
+++ flaskproject/config.py
@@ -0,0 +1,17 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '0cb859f15ca7b98baeb9d4be989f33bb'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+   # SERVER_NAME = 'localhost:5000'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'XXXXXX@gmail.com'
+    MAIL_PASSWORD = 'XXXXXX'
+
+
+
+
+
